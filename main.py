3748444b212fc879f7053e1209f086081e145804
Cassio Rosa <cassioasrosa@gmail.com>
2025-09-23 16:49:53 -0300

    função consultar dados no banco

--- main.py
+++ main.py
@@ -23,28 +23,46 @@ CREATE TABLE IF NOT EXISTS alunos (
 """)
 
 
-#inserir um aluno no banco de dados
-nome = input("Digite o nome do aluno que desejar cadastrar: ")
-cursor.execute(f"""
-INSERT INTO alunos (nome, idade, curso)
-VALUES (?,?,?)
-""", (nome, 20, 'Medicina'))
+# #inserir um aluno no banco de dados
+# nome = input("Digite o nome do aluno que desejar cadastrar: ")
+# cursor.execute(f"""
+# INSERT INTO alunos (nome, idade, curso)
+# VALUES (?,?,?)
+# """, (nome, 20, 'Medicina'))
 
 
-#Podemos cadastrar varias informações no banco de dados
-alunos = [
-    ('Enzo', 22, 'Direito'),
-    ('Murilo', 33, 'Computação'),
-    ('Eduardo', 41, 'Computação'),
-]
+# #Podemos cadastrar varias informações no banco de dados
+# alunos = [
+#     ('Enzo', 22, 'Direito'),
+#     ('Murilo', 33, 'Computação'),
+#     ('Eduardo', 41, 'Computação'),
+# ]
 
-# Executemany permite inserir múltiplas linhas de uma vez só
-cursor.executemany("""
-INSERT INTO alunos (nome, idade, curso)
-VALUES (?,?,?)
-""", alunos)
+# # Executemany permite inserir múltiplas linhas de uma vez só
+# cursor.executemany("""
+# INSERT INTO alunos (nome, idade, curso)
+# VALUES (?,?,?)
+# """, alunos)
 
 
-#Precisamos confirmar as alterações no banco de dados
+# #Precisamos confirmar as alterações no banco de dados
+# conexao.commit()
+
+#Atualizar os dados no banco de dados
+cursor.execute("""
+UPDATE alunos
+SET idade = ?, curso = ?
+WHERE id = ?
+""",(27,"ADS",2))
 conexao.commit()
+print("Dados atualizados com sucesso!")
 
+#Consultar os dados no banco de dados
+cursor.execute("SELECT * FROM alunos")
+#fetchall traz todas as linhas da tabela
+for linha in cursor.fetchall():
+    print(f"ID: {linha[0]}| Nome: {linha[1]}| Idade: {linha[2]}| Curso: {linha[3]}")
+
+cursor.execute("SELECT * FROM alunos WHERE curso = ?", ("Computação",))
+for linha in cursor.fetchall():
+    print(f"ID: {linha[0]}| Nome: {linha[1]}| Idade: {linha[2]}| Curso: {linha[3]}")

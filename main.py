6cbea6eb7b2a7792fa0c333db634bfd905ff510b
Cassio Rosa <cassioasrosa@gmail.com>
2025-09-23 14:31:44 -0300

    Criando a tabela do banco de dados

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 #Importar a biblioteca sqlite3
 import sqlite3
+
+#Cria uma conexão com o banco de dados chamado "escola.db"
+
+conexao = sqlite3.connect("escola.db")
+
+#Criar um objeto "cursor" server para executar os comandos SQL
+cursor = conexao.cursor()
+
+#Criar uma tabela no banco de dados
+cursor.execute("""
+
+CREATE TABLE IF NOT EXISTS alunos (
+               
+    
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    idade INTEGER,
+    curso TEXT
+                
+    )
+""")
+
+print("Tabela criada com sucesso")

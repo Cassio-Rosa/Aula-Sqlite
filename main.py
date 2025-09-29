0fdffc31d5306e52ba154ea28ff602d6de5ec777
Cassio Rosa <cassioasrosa@gmail.com>
2025-09-29 13:33:48 -0300

    add função de deletar

--- main.py
+++ main.py
@@ -49,20 +49,29 @@ CREATE TABLE IF NOT EXISTS alunos (
 # conexao.commit()
 
 #Atualizar os dados no banco de dados
-cursor.execute("""
-UPDATE alunos
-SET idade = ?, curso = ?
-WHERE id = ?
-""",(27,"ADS",2))
+# cursor.execute("""
+# UPDATE alunos
+# SET idade = ?, curso = ?
+# WHERE id = ?
+# """,(27,"ADS",2))
+# conexao.commit()
+# print("Dados atualizados com sucesso!")
+
+# #Consultar os dados no banco de dados
+# cursor.execute("SELECT * FROM alunos")
+# #fetchall traz todas as linhas da tabela
+# for linha in cursor.fetchall():
+#     print(f"ID: {linha[0]}| Nome: {linha[1]}| Idade: {linha[2]}| Curso: {linha[3]}")
+
+# cursor.execute("SELECT * FROM alunos WHERE curso = ?", ("Computação",))
+# for linha in cursor.fetchall():
+#     print(f"ID: {linha[0]}| Nome: {linha[1]}| Idade: {linha[2]}| Curso: {linha[3]}")
+
+
+#Deletando dados no banco
+
+cursor.execute("DELETE FROM alunos WHERE id=?", (2,))
 conexao.commit()
-print("Dados atualizados com sucesso!")
-
-#Consultar os dados no banco de dados
-cursor.execute("SELECT * FROM alunos")
-#fetchall traz todas as linhas da tabela
-for linha in cursor.fetchall():
-    print(f"ID: {linha[0]}| Nome: {linha[1]}| Idade: {linha[2]}| Curso: {linha[3]}")
-
-cursor.execute("SELECT * FROM alunos WHERE curso = ?", ("Computação",))
-for linha in cursor.fetchall():
-    print(f"ID: {linha[0]}| Nome: {linha[1]}| Idade: {linha[2]}| Curso: {linha[3]}")
+#SEMPRE FECHAR A CONEXAOOO
+conexao.close()
+#obrigado :D
